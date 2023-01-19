b2f696266ca9f64830848628f3719d708e31fa84
Eduard Tokarev <etdevios@gmail.com>
2023-01-19 17:21:15 +0300

    Сверстан LaunchScreen

---
+++ ImageFeed/Resources/UIColor+Extesions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extesions.swift
+//  ImageFeed
+//
+//  Created by Eduard Tokarev on 19.01.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBackground: UIColor {UIColor(named: "YPBackground") ?? .clear}
+    static var ypBlack: UIColor {UIColor(named: "YPBlack") ?? .clear}
+    static var ypBlue: UIColor {UIColor(named: "YPBlue") ?? .clear}
+    static var ypGray: UIColor {UIColor(named: "YPGray") ?? .clear}
+    static var ypRed: UIColor {UIColor(named: "YPRed") ?? .clear}
+    static var ypWhite: UIColor {UIColor(named: "YPWhite") ?? .clear}
+    static var ypWhiteAlpha50: UIColor {UIColor(named: "YPWhite(Alpha50)") ?? .clear}
+}
