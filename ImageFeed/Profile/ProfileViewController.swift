814a2cdd356a8e3d944cc1a155d705888b316cea
Eduard Tokarev <etdevios@gmail.com>
2023-02-03 06:50:01 +0300

    added Class ProfileViewController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Eduard Tokarev on 02.02.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var nicknameLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var logoutView: UIButton!
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+    }
+}
